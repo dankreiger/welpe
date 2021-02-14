6c3b8dabb154200613d3dc9ea0cece99d6c76e49
Dan Kreiger <dan@dankreiger.com>
2021-02-14 11:24:18 +0100

    fix(button): update registries in scripts

--- scripts/workspaces/test-publish.sh
+++ scripts/workspaces/test-publish.sh
@@ -1,6 +1,5 @@
-
 #!/usr/bin/env bash
 echo "┏━━━ 📦 AUTO-PUBLISH ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 yarn clean && 
 yarn build && 
-lerna publish --conventional-commits 
+lerna publish --conventional-commits --registry=http://localhost:4873/ 
