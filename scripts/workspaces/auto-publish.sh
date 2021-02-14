062c68a4283b64aa02032ddc22173497d8be98c7
Dan Kreiger <dan@dankreiger.com>
2021-02-14 03:12:08 +0100

    fix(button): add proxy-registry

---
+++ scripts/workspaces/auto-publish.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+echo "┏━━━ 📦 AUTO-PUBLISH ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+lerna publish --conventional-commits
---
+++ scripts/workspaces/proxy-registry.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "┏━━━ 🔥 PROXY-REGISTRY ━━━━━━━"
+npx verdaccio
